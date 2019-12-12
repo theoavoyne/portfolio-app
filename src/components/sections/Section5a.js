0630a465bb649af5f813e15da4fc4acdbbfa3daa
Theophile Avoyne <tavoyne@gmail.com>
2019-12-12 14:37:26 +0100

    Added responsiveness for all sections and components + mobile chevron

--- src/components/sections/Section5a.js
+++ src/components/sections/Section5a.js
@@ -3,18 +3,27 @@ import React from 'react';
 const Section5a = () => (
   <div className="slide">
     <div className="s5-flex">
-      <img
-        src="/images/remood.png"
-        alt="Remood"
-        className="s5-illustration"
-      />
+      <div className="s5-left">
+        <img
+          src="/images/remood.png"
+          alt="Remood"
+          className="s5-illustration"
+        />
+      </div>
       <div className="s5-right">
         <h3 className="s5-title">Remood</h3>
         <p className="s5-description">
           Remood est une plateforme qui analyse votre profil et historique d&apos;écoutes
-          Spotify pour vous générer des playlists sur-mesure et entièrement personnalisables.
+          Spotify pour vous générer des playlists sur-mesure et personnalisables.
         </p>
-        <a href="https://www.remood.io/" className="s5-link" target="_blank">Voir site</a>
+        <a
+          href="https://www.remood.io/"
+          className="s5-link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Voir site
+        </a>
       </div>
     </div>
   </div>
--- src/components/sections/Section5c.js
+++ src/components/sections/Section5c.js
@@ -3,17 +3,26 @@ import React from 'react';
 const Section5c = () => (
   <div className="slide">
     <div className="s5-flex">
-      <img
-        src="/images/binsard.png"
-        alt="Binsard Avocats"
-        className="s5-illustration"
-      />
+      <div className="s5-left">
+        <img
+          src="/images/binsard.png"
+          alt="Binsard Avocats"
+          className="s5-illustration"
+        />
+      </div>
       <div className="s5-right">
         <h3 className="s5-title">Binsard Avocats</h3>
         <p className="s5-description">
           Cabinet d’avocats basé sur Paris 8ème dédié à la défense pénale et au contentieux.
         </p>
-        <a href="https://www.binsard-avocats.com/" className="s5-link" target="_blank">Voir site</a>
+        <a
+          href="https://www.binsard-avocats.com/"
+          className="s5-link"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Voir site
+        </a>
       </div>
     </div>
   </div>
